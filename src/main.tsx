6346b1e580d3eb750e12a84595252b2f6057be1e
Tanishq <93016588+Technishq@users.noreply.github.com>
2026-01-17 20:10:23 +0530

    fixing menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { pdfjs } from 'react-pdf'
 
+pdfjs.disableWorker = true
 createRoot(document.getElementById("root")!).render(<App />);
